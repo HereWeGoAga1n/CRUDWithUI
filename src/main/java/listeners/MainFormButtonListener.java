ff7b0db4fcbdc03678959e68641dcab6aa542391
DeadCrawler <pimonovkostya@gmail.com>
2021-02-16 19:38:37 +0200

    Create update

--- src/main/java/listeners/MainFormButtonListener.java
+++ src/main/java/listeners/MainFormButtonListener.java
@@ -1,12 +1,19 @@
 package listeners;
 
 import org.json.simple.parser.ParseException;
+import project2.cmd.Executable;
+import project2.model.Person;
 import ui.FileDBChooserPanel;
+import ui.NewFileCreator;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import static ui.Container.table;
 import static project2.io.OpenDocument.executable;
@@ -22,8 +29,13 @@ public class MainFormButtonListener implements ActionListener {
     JTextField txtLname;
     JTextField txtAge;
     JTextField txtCity;
+    JTextField fileName;
+    JComboBox fileType;
     public static int returnVal;
     public static FileDBChooserPanel fileDBChooserPanel;
+    Executable executable;
+    ArrayList<Person> arrayList;
+    Person person;
 
     public MainFormButtonListener(JButton btnRead, JButton btnUpdate, JButton btnCreate, JButton btnDelete, JButton btnExit,
                                   JTextField txtId, JTextField txtFname, JTextField txtLname, JTextField txtAge, JTextField txtCity) {
@@ -39,6 +51,11 @@ public class MainFormButtonListener implements ActionListener {
         this.txtCity = txtCity;
     }
 
+    public MainFormButtonListener(JTextField nameField, JComboBox comboBox) {
+        fileName = nameField;
+        fileType = comboBox;
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -49,6 +66,33 @@ public class MainFormButtonListener implements ActionListener {
             long id = table.getActiveRow();
             try {
                 executable.delete(id);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            } catch (ParseException parseException) {
+                parseException.printStackTrace();
+            } catch (ClassNotFoundException classNotFoundException) {
+                classNotFoundException.printStackTrace();
+            }
+        }else if(source == btnSave){
+            person = new Person();
+            arrayList = new ArrayList<>();
+            person.setId(Integer.parseInt(txtId.getText()));
+            person.setFname(txtFname.getText());
+            person.setLname(txtLname.getText());
+            person.setAge(Integer.parseInt(txtAge.getText()));
+            person.setCity(txtCity.getText());
+            arrayList.add(person);
+            NewFileCreator newFileCreator = new NewFileCreator();
+            Path path = Paths.get("C:\\Users\\DeadCrawler\\Desktop", fileName.getText()+ "." + fileType.getActionCommand());
+            File file = new File(String.valueOf(path));
+            try {
+                file.createNewFile();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            try {
+                System.out.println(person.toString());
+                executable.create(arrayList);
             } catch (IOException ioException) {
                 ioException.printStackTrace();
             } catch (ParseException parseException) {
